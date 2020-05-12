8fef28d758dcee03be04a02b7eec9917e57147fc
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-05-12 09:22:13 -0300

    Criando uma aplicação em React (aula 5)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 'use strict'
 
-var sum = require('./App')
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Title = require('./App')
 
-console.log(sum(1, 2))
+ReactDOM.render(
+    React.createElement(Title),
+    document.querySelector('[data-js="app"]')
+)
